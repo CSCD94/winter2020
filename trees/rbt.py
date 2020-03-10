2af28b4162b8658134d8ea24f4a6315b27398676
Zijie(Jay) Wang <wangzijie011@gmail.com>
2020-03-10 17:35:23 -0400

    rbt for comment (Not yet finished)

--- trees/rbt.py
+++ trees/rbt.py
@@ -15,17 +15,26 @@ class RedBlackTree(BST):
         not insert duplicates.
 
         '''
+        if self._root is None:
+            self._root = RBTNode(value, BLACK)
+            return self
+        # when not inserting at a root
+        new_node = RBTNode(value, RED)
+        _insert_leaf(self._root, new_node)
+        _rebalance(new_node)
+        return self
 
-        
 
+    # how is find and depth different from parent?
+    """
     def find(self, value):
         '''Return a RBTNode from this RedBlackTree that contains the value
         value. If there is no such RBTNode in the tree, raise a
         NoSuchValueException.
 
         '''
-
-        pass
+        super().find(value)
+        
 
     def depth(self, value):
         '''Return the depth (the number of RBTNodes on the root-to-node path)
@@ -34,8 +43,8 @@ class RedBlackTree(BST):
         raise a NoSuchValueException.
 
         '''
-
-        pass
+        super().depth(value)
+    """
 
     # Only if you feel like it! :)
     def delete(self, value):
@@ -49,15 +58,81 @@ class RedBlackTree(BST):
 
 
 def _insert_leaf(node, new_node):
-    '''Insert new_node at leaf level in the tree rooted at node.'''
+    '''Insert new_node at leaf level in the tree rooted at node.
+    
+    (?) prerequisite: node is not None
+    '''
+    if new_node < node:
+        if node.left is None:
+            node.left = new_node
+            new_node.parent = node
+        else:
+            _insert_leaf(node.left, new_node)
+    if new_node > node:
+        if node.right is None:
+            node.right = new_node
+            new_node.parent = node
+        else:
+            _insert_leaf(node.right, new_node)
+    return BST(node)
 
-    pass
+"""
+# newly-defined helper function
+def _recolouring(root, node):
+    '''Recolour the node to fix a newly coloured RBT'''
+    if not node == root:
+        node.parent.colour = BLACK
+        node.get_aunt().colour = BLACK
+        node.get_grandparent().colour = RED
+    _recolouring(node.get_grandparent())
+"""
+def _swap_colour(node1, node2):
+    '''Swap the colour of node1 and node2'''
+    node1_colour = node1.colour
+    node1.colour = node2.colour
+    node2.colour = node1_colour
 
 
 def _rebalance(node):
     '''Fix the RedBlackTree root at newly inserted RBTNode node.'''
-
-    pass
+    # when parent is BLACK, we are done;
+    # otherwise, need to fix the tree
+    if node == self._root:
+        self._root.colour = BLACK
+    else:
+        aunt = new_node.get_aunt()
+        grandparent = new_node.get_grandparent()
+        parent = new_node.parent
+        if parent.colour == RED:
+            # Case 1: the aunt of the new_node is RED
+            # use recolouring to fix the tree            
+            if aunt.colour == RED:
+                parent.colour = BLACK
+                aunt.colour = BLACK
+                grandparent.colour = RED            
+                _rebalance(grandparent)
+            # Case 2: the aunt of the new_node is BLACK
+            # use rotation to fix the tree
+            if aunt.colour == BLACK:
+                # left left case:
+                if grandparent.left == parent and parent.left == new_node:
+                    _rotate_right(grandparent)
+                    _swap_colour(parent, grandparent)
+                # left right case:
+                if grandparent.left == parent and parent.right == new_node:
+                    _rotate_left(parent)
+                    _rotate_right(grandparent)
+                    _swap_colour(parent, grandparent)                   
+                # right right case:
+                if grandparent.right == parent and parent.right == new_node:
+                    _rotate_left(grandparent)
+                    _swap_colour(parent, grandparent)
+                # right left case:
+                if grandparent.right == parent and parent.left == new_node:
+                    _rotate_right(parent)
+                    _rotate_left(grandparent)
+                    _swap_colour(parent, grandparent)
+    return self
 
 
 def _rotate_left(node):
@@ -66,8 +141,14 @@ def _rotate_left(node):
     Precondition: node.right is not None
 
     '''
-
-    pass
+    #g = node
+    #p = node.left
+    node.left.parent = node.parent
+    T3 = node.left.right
+    node.left.right = node
+    node.parent = node.left
+    node.left = T3
+    T3.parent = node
 
 
 def _rotate_right(node):
@@ -76,8 +157,14 @@ def _rotate_right(node):
     Precondition: node.right is not None
 
     '''
-
-    pass
+    #g = node
+    #p = node.right
+    node.right.parent = node.parent
+    T3 = node.right.left
+    node.right.left = node
+    node.parent = node.right.left
+    node.right = T3
+    T3.parent = node
 
 
 class RBTNode(BTNode):
@@ -96,21 +183,25 @@ class RBTNode(BTNode):
 
     def get_grandparent(self):
         '''Return the grandparent of this RBTNode, or None if the parent is None.'''
-
-        pass
+        if self.parent is None:
+            return None
+        return self.parent.parent
 
     def get_sibling(self):
         '''Return the sibling of this RBTNode. Return None if this RBTNode is root.'''
-
-        pass
+        if self.parent is None:
+            return None
+        if self < self.parent:
+            return self.parent.right
+        # put if self > self.parent seems more readable?
+        return self.parent.left
 
     def get_aunt(self):
         '''Return a sibling of this RBTNode's parent. Return None if this
         RBTNode is root or if this RBTNode's parent is root.
 
         '''
-
-        pass
+        return self.parent.get_sibling()
 
     def __str__(self):
         '''Return a str representation of this RedBlackTree.'''
